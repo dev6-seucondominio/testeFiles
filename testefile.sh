e4d6add2ec026086af4d2f1f42846295830359b6
Diego F <dev6.seucondominio@gmail.com>
2016-11-04 14:23:25 -0200

    teste 'ls'

--- testefile.sh
+++ testefile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Hello, world!"
 read -p "What is your name? " name
-ls
+echo `ls`
 echo "Hello, ${name}!"
 # bash <(curl -s https://raw.githubusercontent.com/dev6-seucondominio/testeFiles/master/testefile.sh)
